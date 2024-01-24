e11fcbe02226def870387ac36adf26b447c6502d
Davis <daviesnyakiba@gmail.com>
2024-01-24 21:31:31 +0300

    Create basic home page elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 // src/App.js
 import React from 'react';
-import LoginForm from './components/LoginForm';
+// import LoginForm from './components/LoginForm';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <LoginForm />
+      {/* <LoginForm /> */}
+      <Home/>
     </div>
   );
 }
---
+++ client/src/components/Home.js
@@ -0,0 +1,22 @@
+import react from 'react';
+
+function Home () {
+    return(
+        <div>
+            <header>
+                <h1>VOTING SYSTEM</h1>
+            </header>
+            <div>
+                <button>Sign In</button>
+            </div>
+            <div>
+                <p></p>
+            </div>
+            <div>
+                <button>Sign Up</button>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
