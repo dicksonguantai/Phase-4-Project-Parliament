7068188a9cb0440fcf87aa9c5a9573859a88806d
Davis <daviesnyakiba@gmail.com>
2024-01-24 22:46:29 +0300

    Add link to log-in page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,18 +1,24 @@
 import react from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css'; 
 
 function Home () {
     return(
-        <div>
+        <div className = "landing-page">
             <header>
                 <h1>VOTING SYSTEM</h1>
             </header>
-            <div>
-                <button>Sign In</button>
+            <div className="signIn-Button">
+                <Link to="/Login">
+                    <button>Sign In</button>
+                </Link>
             </div>
-            <div>
-                <p></p>
+            <div className="content-text">
+                <p>Legislative bodies often face challenges in efficiently monitoring bills, sponsors, votes, and outcomes. Existing systems may lack simplicity or be cumbersome to manage, necessitating a streamlined and accessible tool.</p>
+                <h2>About</h2>
+                <p>The "Parliamentary Bill Tracker" is a user-friendly system developed using Flask and React, designed to monitor bills submitted to parliament. It maintains comprehensive records of sponsoring MPs, vote tallies, and legislative outcomes, providing a clear overview within a manageable platform.</p>
             </div>
-            <div>
+            <div className="signup-button">
                 <button>Sign Up</button>
             </div>
         </div>
