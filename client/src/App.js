2f04662b7494cfcad91e8cafb59979df46d456ae
Davis <daviesnyakiba@gmail.com>
2024-01-24 22:59:57 +0300

    Update Routes mapped by sign in and sign-up button

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 // src/App.js
 import React from 'react';
-// import LoginForm from './components/LoginForm';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
 import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      {/* <LoginForm /> */}
-      <Home/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<LoginForm/>}/>
+        <Route path="/logout" element={<LoginForm/>}/>
+      </Routes>
+    </Router>
   );
 }
 
