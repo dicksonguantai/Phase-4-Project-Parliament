1c2cc154ec20a20eb23e1b3ba44fc864901f64fc
Samuel Muli <samuelmuli95@gmail.com>
2024-01-25 12:47:53 +0300

    Add Login and Logout and Checksession resources

--- server/app.py
+++ server/app.py
@@ -66,10 +66,66 @@ class Signup(Resource):
         except IntegrityError:
             db.session.rollback()
             return {'message': 'An error occurred'}, 422
+        
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            if user:
+                return {
+                    'id': user.id,
+                    'first_name': user.first_name,
+                    'last_name': user.last_name,
+                    'email': user.email,
+                    'role': user.role
+                }, 200
+            else:
+                return {'message': 'User not found'}, 404
+            
+        else:
+            return {'message': 'Please log in'}, 401
+
+
+class Login(Resource):
+    def post(self):
+        email = request.json['email']
+        password = request.json['password']
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return {
+                'id': user.id,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'email': user.email,
+                'role': user.role
+            }, 201
+        else:
+            return {'message': 'Invalid email or password'}, 401
+        
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+
+        return {
+            'message': 'Logged out successfully'
+        }, 200
+    
+
+
 
 
 
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
