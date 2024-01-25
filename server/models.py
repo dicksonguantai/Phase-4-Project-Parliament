e257ecaa970925813cd29c9160aa6e348c77a165
Samuel Muli <samuelmuli95@gmail.com>
2024-01-25 10:24:25 +0300

    Add password hashing and authentication methods for user security

--- server/models.py
+++ server/models.py
@@ -42,6 +42,18 @@ class User(db.Model):
     password = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
 
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('password_hash is not a readable attribute')
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password.encode('utf-8'))
+
     def __repr__(self):
         return f'<User id={self.id} name={self.name} role={self.role} email={self.email}>'
 
