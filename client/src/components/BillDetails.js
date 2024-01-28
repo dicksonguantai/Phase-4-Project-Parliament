abb8cde87806b2054b0b051da9df6bc06176219e
Davis <daviesnyakiba@gmail.com>
2024-01-28 13:48:47 +0300

    Check if normal user or an mp

--- client/src/components/BillDetails.js
+++ client/src/components/BillDetails.js
@@ -4,8 +4,18 @@ import { useParams } from "react-router-dom";
 function BillDetails ()  {
   const { bill_id } = useParams();
   const [billDetails, setBillDetails] = useState({});
+  const [userRole, setUserRole] = useState('user');
 
   useEffect(() => {
+
+    fetch('/check_session')
+    .then((response) => response.json())
+    .then((data) => {
+      setUserRole(data.role);
+    })
+    .catch((error) => console.error(error));
+
+
     fetch(`/bills/${bill_id}`)
       .then((r) => {
         if (r.ok) {
@@ -33,7 +43,13 @@ function BillDetails ()  {
       <p>{billDetails.outcome_status}</p>
       <p>{billDetails.up_votes}</p>
       <p>{billDetails.down_votes}</p>
+      {userRole === 'mp' && (
+        <button type="button" onClick={handleVote}>
+          Vote
+        </button>
+      )}
 
+      {userRole !== 'mp' && <p>You can only view the details.</p>}
 
     </div>
   );
