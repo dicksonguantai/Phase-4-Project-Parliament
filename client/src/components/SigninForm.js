e44af8cb52ddc4d5f8f4f7345b46c633ac599201
Ahmed yussuf Abdullahi <ahmedyussuf917@gmail.com>
2024-01-29 09:09:27 +0300

    changed frontend design

--- client/src/components/SigninForm.js
+++ client/src/components/SigninForm.js
@@ -2,7 +2,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
-
 const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -30,13 +29,18 @@ const LoginForm = () => {
     }
   };
 
+  const handleForgotPassword = () => {
+    navigate(`/resetpassword/${email}`);
+  };
+
   return (
-    <div className="signup-box"> {/* Use the same styling as SignupForm */}
+    <div className="signup-box">
       <h2>Login</h2>
 
       <div className="form-group">
         <label>Email:</label>
         <input
+          className="input-field"
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
@@ -46,6 +50,7 @@ const LoginForm = () => {
       <div className="form-group">
         <label>Password:</label>
         <input
+          className="input-field"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
@@ -53,9 +58,15 @@ const LoginForm = () => {
       </div>
 
       <div className="form-group">
-        <button type="button" onClick={handleLogin}>
+        <button className="login-button" type="button" onClick={handleLogin}>
           Login
         </button>
+      </div>
+
+      <div className="forgot-password">
+        <Link to="#" onClick={handleForgotPassword}>
+          Forgot Password?
+        </Link>
       </div>
 
       <p>
